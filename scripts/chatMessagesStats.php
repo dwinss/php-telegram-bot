788937e112918616d77e468a4a70a5da6bdded78
root <root@node03.4nmv.ru>
2019-10-27 02:59:17 +0300

    Added /stat_chats command

--- scripts/chatMessagesStats.php
+++ scripts/chatMessagesStats.php
@@ -32,7 +32,8 @@ if(mb_substr($_TEXT, 0, 6, 'utf-8') == '/stats')
 				$str .= isset($c1) ? '<i>Всего сообщений за '.$res[1].' ч:</i> '.$c1.PHP_EOL : '';
 				$str .= '<i>Всего сообщений в этом чате:</i> '.$c2.PHP_EOL;
 				$str .= '<code>/stats N</code> - статистика за последние N часов'.PHP_EOL;
-				$str .= 'Глобальная статистика - /globalstats'.PHP_EOL;
+				$str .= 'Глобальная статистика пользователей - /globalstats'.PHP_EOL;
+				$str .= 'Глобальная статистика чатов - /stat_chats'.PHP_EOL;
 				
 				sendMessage($_CHAT['id'], $str, 'HTML', $_MESS['message_id']);
 			}
@@ -48,4 +49,20 @@ if(preg_match('#^/globalstats#iu', $_TEXT))
 			}
 		
 		sendMessage($_CHAT['id'], $str, 'HTML', $_MESS['message_id']);
+	}
+
+if(preg_match('#^/stat_chats#iu', $_TEXT))
+	{
+		// sendMessage($_CHAT['id'], '+');
+		$q = mysql_query("SELECT `messages`.`id_chat`, `tg_chats`.`title` as title, count(`messages`.`message`) AS ccc from `messages` LEFT JOIN `tg_chats` USING(`id_chat`) WHERE `messages`.`chat_name` != 'ЛС' GROUP BY title ORDER BY `ccc` DESC LIMIT 10");
+		
+		$str = '*10 самых активных чатов*'.PHP_EOL.PHP_EOL;
+		
+		while($ress = mysql_fetch_assoc($q))
+			{
+				$str .= '`'.$ress['title'].'`: *'.$ress['ccc'].'*'.PHP_EOL;
+				
+			}
+			
+		sendMessage($_CHAT['id'], $str, 'Markdown', $_MESS['message_id']);
 	}
